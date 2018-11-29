db94e5562d7b1378c1524b8b5a9eb043ef96c4dd
Milly Huckle <millycyb@outlook.com>
2018-11-29 13:46:37 +0000

    initial commit

---
+++ practise/exl.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+print("Hello world")
+print("Hello again")
+print("I like typing like this")
---
+++ practise/hello.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+age = 5 #here we are giving the variable age a value of 5
+like ="painting" #here we are giving the variable like a value of painting
+age_description = "My age is {0} and I like {1}.".format(age,like) #here we are giving the variable age_description a value of a string that includes the other two variables we created before hand. The way the string can retrieve these variables is through the format constant. This is similar to an array.
+print(age_description) #this is where the console will show the variable and it's value.
+
+print(10/3)
+print(10%3)
+
