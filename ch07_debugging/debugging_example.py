cf3dadeb971d6e3ef2cf5324e12b610c23d835b9
Milly Huckle <millycyb@outlook.com>
2018-12-18 16:48:00 +0000

    putting debugging into practice

--- ch07_debugging/debugging_example.py
+++ ch07_debugging/debugging_example.py
@@ -17,3 +17,22 @@ print(type(userInput))
 #CAN USE THE TYPE METHOD TO CHECK WHAT DATA
 #TYPE THE INPUT IS. IF YOU ARE GIVING AN INTEGER
 #AS THE INPUT FOR A STRING INPUT, IT WONT WORK.
+
+userInput = input("Please give a number ")
+
+def simpleOperation(userInput):
+    result = userInput - 2
+    return result
+def nestedOperation():
+    result = simpleOperation(userInput)
+    result2 = result * 2
+    return result2
+
+result = simpleOperation(userInput)
+result2 = nestedOperation()
+print(result2)
+
+#The first button in the debug panel is for you to start runing your code until the break point.
+#The second button allows you to run your code line-by-line until the breakpoint
+#The third one is for stepping into the sections (class and functions) that you would like to dig into more and the fourth button is for you to step out when you feel that the error is not related to the current section.
+#The fifth button is for you to go to the next breakpoint, and the last square shaped button is for you to exit debugging mode and go back to normal coding mode.
